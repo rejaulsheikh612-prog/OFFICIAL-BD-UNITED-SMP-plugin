9413802933e3ab8d34a1d0feb024adb0a45d48f0
rejaulsheikh612-prog <rejaulsheikh612@gmail.com>
2025-12-16 18:38:51 +0600

    Update JoinListener.java

--- src/main/java/bdunited/plugin/JoinListener.java
+++ src/main/java/bdunited/plugin/JoinListener.java
@@ -7,13 +7,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class JoinListener implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        var p = e.getPlayer();
-
-        p.sendMessage("§6BD UNITED SMP — Official Rules");
-        p.sendMessage("§e1. No Hacks, Cheats, or X-Ray");
-        p.sendMessage("§e2. No Violence or Toxic Behavior");
-        p.sendMessage("§e3. No Bad or Offensive Language");
-        p.sendMessage("§e4. Follow All Server Rules");
+    public void onJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Rules.TEXT);
     }
-          }
+}
